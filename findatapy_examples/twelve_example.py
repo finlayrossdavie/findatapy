1a1c282dee4c7f80cf407510a8ca0167c692e87d
finlayrossdavie <39996934+finlayrossdavie@users.noreply.github.com>
2024-11-09 18:29:46 +0000

    most recent updates

--- findatapy_examples/twelve_example.py
+++ findatapy_examples/twelve_example.py
@@ -19,83 +19,16 @@ if __name__ == '__main__':
     market = Market(market_data_generator=MarketDataGenerator())
 
     # Get the first release for GDP and also print the release date of that
-    md_request = MarketDataRequest(
-        start_date="01 Jun 2000",
-        # start date (download data over past decade)
-        data_source='twelve',  # use Twelve Data as source 
-        tickers=['BLK'],  # ticker
-        fields=['actual-release', 'release-date-time-full'],
-        # which fields to download
-        vendor_tickers=['BLK'],  # ticker (FRED)
-        vendor_fields=['actual-release',
-                       'release-date-time-full'])  # which FRED fields to download
+
+    md_request = MarketDataRequest(start_date="01 Jan 2001",
+                                   finish_date="01 Dec 2008",
+                                   tickers=["BLK"],
+                                   vendor_tickers=["BLK"],
+                                   fields=["close"],
+                                   data_source="twelve")
+    
     df = market.fetch_market(md_request)
 
-
+    
+    
     print(df)
-
-    # Compare the close and actual release of US GDP (and the final)
-    md_request = MarketDataRequest(
-        start_date="01 Jun 2000",
-        # start date (download data over past decade)
-        data_source='twelve',  # use ALFRED/FRED as data source
-        tickers=['US GDP'],  # ticker
-        fields=['actual-release', 'close'],  # which fields to download
-        vendor_tickers=['GDP'],  # ticker (FRED)
-        vendor_fields=['actual-release',
-                       'close'])  # which FRED fields to download
-
-    df = market.fetch_market(md_request)
-
-    from chartpy import Chart, Style
-
-    style = Style(title="US GDP first vs last")
-
-    Chart().plot(df, style=style)
-
-    # Get the change NFP SA (need to calculate that from the acutal-release and first-revision)
-    md_request = MarketDataRequest(
-        start_date="01 Jun 2000",
-        # start date (download data over past decade)
-        data_source='twelve',  
-        tickers=['US NFP'],  # ticker
-        fields=['actual-release', 'first-revision', 'release-date-time-full'],
-        # which fields to download
-        vendor_tickers=['PAYEMS'],  # ticker (FRED)
-        vendor_fields=['actual-release', 'first-revision',
-                       'release-date-time-full'])  # which FRED fields to download
-
-    df = market.fetch_market(md_request)
-
-    # calculate the headline change in NFP
-    df['US NFP change'] = df['US NFP.actual-release'] - df[
-        'US NFP.first-revision'].shift(1)
-
-    print(df)
-
-    from chartpy import Chart, Style
-    import pandas
-
-    style = Style(title="US NFP change (actual)")
-
-    df1 = pandas.DataFrame(df['US NFP change'])
-
-    Chart().plot(df1, style=style)
-
-    # Get release times on their own
-    # Get the change NFP SA
-    # need to calculate that from the acutal-release and first-revision)
-    md_request = MarketDataRequest(
-        start_date="01 Aug 2013",
-        finish_date="30 Nov 2019",
-        data_source='twelve',
-        tickers=['US NFP'],
-        fields=['release-date-time-full'],
-        vendor_tickers=['PAYEMS'],
-        vendor_fields=['release-date-time-full'])
-
-    market = Market(market_data_generator=MarketDataGenerator())
-
-    df_nfp = market.fetch_market(md_request)
-
-    print(df_nfp)
